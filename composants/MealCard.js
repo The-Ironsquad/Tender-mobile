1ec0f4d31356fdb766a03e1918affc69ac7d95f9
Shengwei LI <51034296+sw-li@users.noreply.github.com>
2023-11-09 16:03:15 +0100

    MealCard created

--- composants/MealCard.js
+++ composants/MealCard.js
@@ -2,10 +2,11 @@ import { StyleSheet, Text, View, Image } from 'react-native'
 import React from 'react'
 
 export default function MealCard({meal}) {
+  const {strMealThumb,strMeal,strCategory} = meal
   return (
     <View>
-      <Text>{meal.strMealThumb}</Text>
-      <Image source={{uri:meal.strMealThumb}}/>
+      <Text>{strMeal}</Text>
+      <Image source={{uri:strMealThumb}}/>
     </View>
   )
 }
